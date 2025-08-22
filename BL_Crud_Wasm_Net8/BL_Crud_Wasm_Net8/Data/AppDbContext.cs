b36a110b633ad13a4c5f4d0f73c80f2c66a9abfc
alain-mathy <alain_mathy@hotmail.com>
2025-08-22 16:09:18 +0200

    Add AppDbContext -> Server

---
+++ BL_Crud_Wasm_Net8/BL_Crud_Wasm_Net8/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BL_Crud_Wasm_Net8.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BL_Crud_Wasm_Net8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<VideoGame> VideoGames { get; set; }
+    }
+}
