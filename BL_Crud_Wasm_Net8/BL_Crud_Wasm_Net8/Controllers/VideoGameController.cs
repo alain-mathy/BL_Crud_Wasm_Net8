cde5c22ac55507b5b4f222e32d907e94232b6e72
alain-mathy <alain_mathy@hotmail.com>
2025-08-23 08:21:49 +0200

    Add CRUD to VideoGameController -> Server

--- BL_Crud_Wasm_Net8/BL_Crud_Wasm_Net8/Controllers/VideoGameController.cs
+++ BL_Crud_Wasm_Net8/BL_Crud_Wasm_Net8/Controllers/VideoGameController.cs
@@ -22,5 +22,59 @@ namespace BL_Crud_Wasm_Net8.Controllers
         {
             return await _context.VideoGames.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VideoGame>> GetGameById(int id)
+        {
+            var result = await _context.VideoGames.FirstOrDefaultAsync(x => x.Id == id);
+            if(result is null)
+            {
+                return NotFound("Game not found");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<VideoGame>> DeleteGame(int id)
+        {
+            var result = await _context.VideoGames.FirstOrDefaultAsync(x => x.Id == id);
+            if (result is null)
+            {
+                return NotFound("Game not found");
+            }
+
+            _context.VideoGames.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<VideoGame>> UpdateGame(int id, VideoGame game)
+        {
+            var result = await _context.VideoGames.FirstOrDefaultAsync(x => x.Id == id);
+            if (result is null)
+            {
+                return NotFound("Game not found");
+            }
+
+            result.Title = game.Title;
+            result.Publisher = game.Publisher;
+            result.ReleaseYear = game.ReleaseYear;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddGame(VideoGame game)
+        {
+            _context.VideoGames.Add(game);
+            await _context.SaveChangesAsync();
+
+            return Ok(game);
+        }
     }
 }
